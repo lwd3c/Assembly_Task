1f5ab852a052e286297853554bb009df99c6bb7b
lwd3c <162901533+lwd3c@users.noreply.github.com>
2024-11-01 09:01:47 +0700

    Upload from local

--- Sources/Thi/ChuoiCon.asm
+++ Sources/Thi/ChuoiCon.asm
@@ -3,62 +3,107 @@ include Irvine32.inc
 ; tim kiem 1 chuoi con m ki tu trong xau n ki tu
 
 .data
-	tb DB "Nhap chuoi ban dau: ", 0
-	msg BYTE 256 DUP(?)
-	tb1 DB "Chuoi ban dau: ", 0
-	tb2 DB "Nhap vao gia tri x: ", 0
-	val1 DWORD ?		; x
-	tb3 DB "Nhap vao gia tri y: ", 0
-	val2 DWORD ?		; y
-	tb4 DB "Chuoi con: ", 0
-	msg2 BYTE ?		
-	
+	msg BYTE "Nhap chuoi ban dau: ", 0
+	msg1 BYTE "Nhap chuoi con can tim: ", 0
+	msg3 BYTE "Tim thay chuoi con trong chuoi ban dau !", 0
+	msg4 BYTE "Khong tim thay chuoi con trong chuoi ban dau !", 0
+	string BYTE 200 DUP(?)
+	sub_string BYTE 200 DUP(?)		
+	leng1 DWORD ?			; n
+	leng2 DWORD ?			; m
+
 .code
 	main proc
 	; nhap chuoi ban dau
-		lea edx, tb
-		call writestring
 		lea edx, msg
-		mov ecx, 256
+		call writestring
+		lea edx, string
+		mov ecx, 200
 		call readstring
-	; in chuoi ban dau
-		lea edx, tb1
+		mov leng1, eax
+
+	; nhap chuoi con
+		lea edx, msg1
 		call writestring
-		lea edx, msg
-		call writestring
+		lea edx, sub_string
+		mov ecx, 200
+		call readstring
+		mov leng2, eax
+
+	; goi ham searchString
+		lea edi, string
+		lea esi, sub_string
+		call searchString
+
+	; hien thi ket qua tim kiem
+		cmp ebx, 0
+		jz NotFound
+
+	; Found
+		lea edx, msg3
+		call writeString
+		jmp Thoat
+
+	NotFound:
+		lea edx, msg4
+		call writeString
+
+	Thoat:
 		call crlf
-	; nhap x
-		lea edx, tb2
-		call writestring
-		call readDec
-		mov val1, eax
-	; nhap y
-		lea edx, tb3
-		call writestring
-		call readdec
-		mov val2, eax
-	; goi ham substring
-		lea esi, msg
-		lea edi, msg2
-		call SubString
-	; in ra chuoi con
-		lea edx, tb4
-		call writestring
-		lea edx, msg2
-		call writestring
 		exit
 	main endp
 
-	SubString proc
-		add esi, val1		; dua con tro toi vi tri bat dau
-		mov ecx, val2
-	L1:	
-		mov al, [esi]
-		mov [edi], al
-		inc esi
-		inc edi
-		loop L1
-		ret
-	SubString endp
+; ham tim kiem sub string
+	searchString proc
+		push esi
+		push edi
+		mov eax, 0			; vi tri ban dau
+		mov ecx, leng1			
+
+	; tu tung vi tri trong eax
+	Next:
+		push eax				; luu vi tri hien tai
+		lea edi, string
+		add edi, eax			; vi tri hien tai
+		lea esi, sub_string		; tro ve vi tri ban dau chuoi con
+		push ecx
+		mov ecx, leng2			; vong lap chuoi con: m
+
+		Check:
+			cmp ecx, 0		; neu chay het chuoi con -> tim thay 
+			jz End_Match
+
+			mov al, [edi]		; ki tu dau tien 
+			cmp al, [esi]		; so sanh voi ki tu dau tien chuoi con
+			jne NoMatch		; neu khong khop, toi ki tu thu 2 cua chuoi ban dau
+			
+			inc edi
+			inc esi
+			dec ecx
+			jmp Check
+
+		; khong khop
+		NoMatch:
+			pop ecx			; lay lai gia tri vong lap ngoai
+			pop eax			; lay lai gia tri vi tri hien tai
+			inc eax			; tang len vi tri tiep theo
+			dec ecx
+			cmp ecx, 0		; neu het chuoi ban dau thi in ra ket qua tim kiem
+			jnz Next			
+
+			mov ebx, 0	
+			jmp Return
+					
+		; tim thay chuoi con
+		End_Match:	
+			pop ecx
+			pop eax
+			mov ebx, 1
+
+		Return:
+			pop edi
+			pop esi
+			ret
+	searchString endp
 
 end main
