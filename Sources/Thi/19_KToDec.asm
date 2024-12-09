08852ffdd1fa694cf8b15e5b6897a9ea554c5546
lwd3c <162901533+lwd3c@users.noreply.github.com>
2024-12-09 20:43:39 +0700

    Upload from local

--- Sources/Thi/19_KToDec.asm
+++ Sources/Thi/19_KToDec.asm
@@ -3,111 +3,151 @@ include Irvine32.inc
 ; chuyen doi co so k sang co so 10: N(10) = an * k^n + a(n-1) * k^(n-1) + ... + a1 * k^1 + a0 * k^0
 
 .data
-    msg BYTE "Nhap k: ", 0
-    msg2 BYTE "Nhap Base k value: ", 0
-    msg3 BYTE "Decimal value: ", 0
+    msg BYTE "Nhap Base: ", 0
+    msg2 BYTE "Nhap so k: ", 0
+    msg3 BYTE "So k trong co so 10: ", 0
+    msg4 BYTE "So nhap vao chua ki tu khong hop le!", 0
     base DWORD ?				; co so 
-    k_value BYTE 20 DUP(?)		; so ban dau
+    k_value BYTE 50 DUP(32)		; so ban dau
     d_value DWORD 0				; so he thap phan
 
 .code
-	main PROC
-	; nhap co so k
+	main proc
+	; nhap base
 		lea edx, msg
 		call WriteString
 		call ReadDec
 		mov base, eax    
     
-	; nhap so ban dau
+	; nhap so ban dau k
 		lea edx, msg2
 		call WriteString
 		lea edx, k_value
-		mov ecx, 20
+		mov ecx, 50
 		call ReadString
-
+		
 	; chuyen doi ki tu ve so
 		lea esi, k_value
-		call UpperCharToValue
-  
+		call ConvertCharToValue
+	
+	; chuyen doi co so
+		lea esi, k_value
+		mov eax, base
+		call ConvertToDecimal
+
+	; hien thi ket qua
+		lea edx, msg3
+		call WriteString
+		mov eax, d_value
+		call writeDec
 
 	exit
 	main endp
 
-; ki tu so
-	NumberToValue proc
-		mov ecx, 9
-		Number:
-			push ecx
-			mov al, [esi]
-			cmp al, 0
-			jz End_Convert
-			sub al, '0'
-			L1:	
-				cmp al, cl
-				je Convert
-				dec ecx
-				jmp L1
-
-		Convert:
-			mov [esi], al
-			inc esi
-			pop ecx
-			jmp Number
+; chuyen ki tu ve so
+	ConvertCharToValue proc
+	    push esi                
+	    ConvertLoop:
+		   mov al, [esi]		
+		   cmp al, 0
+		   je Remove
+		   cmp al, 32			; kiem tra ket thuc chuoi
+		   je EndConvert
 		
-		End_Convert:
-			ret
-	NumberToValue endp
+		; kiem tra chu so
+		   cmp al, '0'			; 30h
+		   jl InvalidChar        
+		   cmp al, '9'
+		   jle IsNumber   
+		   
+		; kiem tra chu in hoa
+		   cmp al, 'A'			; 41h
+		   jl InvalidChar        
+		   cmp al, 'Z'
+		   jle IsUpperChar     
 
-; ki tu in hoa
-	UpperCharToValue proc
-		mov ecx, 26
-		UpperChar:
-			push ecx
-			mov al, [esi]
-			cmp al, 0
-			jz End_Convert
-			add al, 10
-			sub al, 'A'
-			L1:	
-				cmp al, cl
-				je Convert
-				dec ecx
-				jmp L1
+		; kiem tra chu in thuong
+		   cmp al, 'a'			; 61h
+		   jl InvalidChar        
+		   cmp al, 'z'
+		   jle IsLowerChar    
+		   jmp InvalidChar
 
-		Convert:
-			mov [esi], al
-			inc esi
-			pop ecx
-			jmp UpperChar
-		
-		End_Convert:
-			ret
-	UpperCharToValue endp
+	    Remove:
+	        mov al, 32
+		   mov [esi], al
+		   inc esi
+		   mov [esi], al
+		   jmp EndConvert
 
-; ki tu in thuong
-	UpperCharToValue proc
-		mov ecx, 26
-		UpperChar:
-			push ecx
-			mov al, [esi]
-			cmp al, 0
-			jz End_Convert
-			add al, 10
-			sub al, 'a'
-			L1:	
-				cmp al, cl
-				je Convert
-				dec ecx
-				jmp L1
+	    IsNumber:
+		   sub al, '0'         
+		   jmp StoreValue
 
-		Convert:
-			mov [esi], al
-			inc esi
-			pop ecx
-			jmp UpperChar
-		
-		End_Convert:
-			ret
-	UpperCharToValue endp
+	    IsUpperChar:
+		   sub al, 'A'       
+		   add al, 10
+		   jmp StoreValue
+
+	    IsLowerChar:
+		   sub al, 'a'       
+		   add al, 10
+		   jmp StoreValue
+
+	    StoreValue:
+		   mov [esi], al   
+		   inc esi             
+		   jmp ConvertLoop
+
+	    InvalidChar:
+		   lea edx, msg4
+		   call writeString
+		   exit
+
+	    EndConvert:
+		   pop esi             
+		   ret
+	ConvertCharToValue endp
+
+; tinh gia tri Dec: N(10) = an * k^n + a(n-1) * k^(n-1) + ... + a1 * k^1 + a0 * k^0
+	ConvertToDecimal proc
+	    push esi
+	    push eax                
+	    xor ecx, ecx         ; do dai chuoi
+	    xor ebx, ebx		; ket qua
+
+	FindLength:
+	    cmp byte ptr [esi + ecx], ' '		; kiem tra ket thuc chuoi
+	    je StartConvert
+	    inc ecx
+	    jmp FindLength
+
+	StartConvert:
+	    dec ecx                ; vi tri so cuoi cung        
+
+	ConvertLoop:
+	    movzx eax, byte ptr [esi]		; gia tri tai vi tri hien tai
+	    mov edi, base				
+	    mov ebx, ecx				; chi so luy thua
+	    cmp ebx, 0					; chi so bang 0 thi khong thuc hien luy thua
+	    je AddValue
+
+	PowerLoop:			; tinh luy thua
+	    mul edi
+	    dec ebx
+	    cmp ebx, 0			; luy thua ve 0 thi thoat vong lap
+	    jnz PowerLoop
+
+	AddValue:				; luu gia tri sau chuyen doi
+	    add d_value, eax  
+	    dec ecx
+	    inc esi
+	    cmp byte ptr [esi], 32
+	    jnz ConvertLoop
+
+	    pop eax                
+	    pop esi
+	    ret
+	ConvertToDecimal endp
 
 end main
--- Sources/Thi/26_DecToLaMa.asm
+++ Sources/Thi/26_DecToLaMa.asm
@@ -1,133 +1,95 @@
 include Irvine32.inc
 
-; chuyen doi so La Ma sang so he 10
-
 .data
-	array BYTE 256 DUP(?)
-	numArray DWORD 256 DUP (?)
-	leng DWORD ?
-	sum DWORD 0
-	tb BYTE "Nhap vao so La Ma (chi chua I, V, X, L, C, D, M): ", 0
-	tb1 BYTE "So La Ma vua nhap la: ", 0
-	tb2 BYTE "Gia tri thap phan cua so La Ma: ", 0 
-	tb3 BYTE "Ki tu dau vao khong hop le !", 0
+     msg1 BYTE "Nhap vao so he 10: ",0
+     msg2 BYTE "So La Ma tuong ung: ",0
+     num BYTE 50 DUP(0)
 
 .code
-	main proc
-	; nhap vao so La Ma
-		lea edx, tb
-		call writestring
-		lea edx, array
-		mov ecx, 256
-		call readString
-		mov leng, eax
-		
-	; in so vua nhap
-		lea edx, tb1
-		call writestring
-		lea edx, array
-		call writestring
-	
-	; tinh gia tri thap phan moi ki tu La Ma
-		lea esi, array				; mang array luu gia tri ban dau
-		lea edi, numArray			; mang numArray luu ket qua
-		call EachValue
+main proc
+    lea edx, msg1
+    call writeString
+    call readDec
 
-	; in gia tri thap phan cua so La Ma
-		lea edx, tb2
-		call writestring
-		lea edi, numArray
-		call SumValue
-		
-		call crlf
-		exit
-	main endp
+    mov ebx, eax
+    lea edi, num 
+    call Compare
 
-	EachValue proc
-		mov ecx, leng				; ecx = chieu dai mang
-	L1:
-		cmp ecx, 0				; neu ecx = 0 thi in ra ket qua
-		je Thoat
-		mov al, [esi]				; so sanh tung phan tu voi tung ki tu La Ma
-		cmp al, "I"
-		je case1
-		cmp al, "V"
-		je case2
-		cmp al, "X"
-		je case3
-		cmp al, "L"
-		je case4
-		cmp al, "C"
-		je case5
-		cmp al, "D"
-		je case6
-		cmp al, "M"
-		je case7
-		call crlf
-		lea edx, tb3				; neu khac cac ki tu La Ma -> hien thi loi va thoat chuong trinh
-		call writestring
-		exit
-	Next:
-		mov [edi], eax				; luu vao mang ket qua
-		dec ecx					
-		add esi, TYPE array			; tro toi phan tu tiep theo
-		add edi, TYPE numArray
-		jmp L1
+    lea edx, msg2
+    call writeString
+    lea edx, num 
+    call writeString
 
-	case1:						; luu cac gia tri thap phan cua so La Ma tuong ung vao eax
-		mov eax, 1
-		jmp Next
-	case2:
-		mov eax, 5
-		jmp Next
-	case3:
-		mov eax, 10
-		jmp Next
-	case4:
-		mov eax, 50
-		jmp Next
-	case5:
-		mov eax, 100
-		jmp Next
-	case6:
-		mov eax, 500
-		jmp Next
-	case7:
-		mov eax, 1000
-		jmp Next
-	
-	Thoat:
-		call crlf
-		ret
-	EachValue endp
+    exit
+main endp
 
-	SumValue proc
-		mov esi, leng
-		dec esi
-		mov eax, [edi + esi * 4]			; luu gia tri cuoi cung vao sum
-		mov sum, eax
-	L3:
-		dec esi	
-		cmp esi, 0			; neu chay het mang thi in ket qua
-		jl Ketqua
+Compare proc
+    mov ecx, 1000   
+    mov edx, 'M'         
+    call Convert
+    
+    mov ecx, 900
+    mov edx, 'MC'
+    call Convert
+    
+    mov ecx, 500
+    mov edx, 'D'
+    call Convert
+    
+    mov ecx, 400
+    mov edx, 'DC'
+    call Convert
+    
+    mov ecx, 100
+    mov edx, 'C'
+    call Convert
+    
+    mov ecx, 90
+    mov edx, 'CX'
+    call Convert
+    
+    mov ecx, 50
+    mov edx, 'L'
+    call Convert
+    
+    mov ecx, 40
+    mov edx, 'LX'
+    call Convert
+    
+    mov ecx, 10
+    mov edx, 'X'
+    call Convert
+    
+    mov ecx, 9
+    mov edx, 'XI'
+    call Convert
+    
+    mov ecx, 5
+    mov edx, 'V'
+    call Convert
+    
+    mov ecx, 4
+    mov edx, 'VI'
+    call Convert
+    
+    mov ecx, 1
+    mov edx, 'I'
+    call Convert
 
-		mov ebx, [edi + esi * 4]			; phan tu hien tai
-		mov edx, [edi + esi * 4 + 4]		; phan tu ke tiep
+    ret
+Compare endp
 
-		cmp ebx, edx		; so sanh 2 phan tu ke tiep, neu nho hon thi thuc hien phep tru
-		jl Subtract
-		add sum, ebx		; nguoc lai, thuc hien phep cong					
-		jmp L3
-		
-	Subtract:
-		sub sum, ebx
-		jmp L3
-
-	Ketqua:
-		mov eax, sum		; in ra ket qua
-		call writedec
-
-		ret
-	SumValue endp
+Convert proc
+    L1:
+        cmp ebx, ecx          ; so sanh so nhap vao voi tung gia tri La Ma    
+        jl Thoat               
+        sub ebx, ecx          ; neu lon hon, tru di va luu gia tri La Ma tuong ung vao mang
+        mov [edi], edx           
+        inc edi              
+        jmp L1
+        
+    Thoat:
+        ret
+Convert endp
 
 end main
